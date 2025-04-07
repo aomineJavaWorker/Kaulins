01ce3a79618fea93a371925cb51a43f86fd21168
Denisenko <aominebas2006@gmail.com>
2025-04-07 10:05:52 +0300

    Masivs ar skaitliem

--- src/Kaulins/Kaulins.java
+++ src/Kaulins/Kaulins.java
@@ -1,12 +1,16 @@
 package Kaulins;
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
+
+import Uzd1.Auto;
 
 public class Kaulins {
 
 	public static void main(String[] args) {
 		int skaitlis, reizes;
+		ArrayList<Integer> skaitli = new ArrayList<>();
 		Scanner scan = new Scanner(System.in);
 		Random rand = new Random();
 		skaitlis = rand.nextInt(6)+1;
@@ -15,8 +19,10 @@ public class Kaulins {
 		for(int i=0; i<reizes;i++) {
 			skaitlis = rand.nextInt(6)+1;
 			System.out.println("Uzkrita skaitlis: "+skaitlis);
+			skaitli.add(skaitlis);
 			
 		}
+		System.out.println(skaitli);
 
 	}
 
